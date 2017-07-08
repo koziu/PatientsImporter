2078f363bc2d732c6113c409d78a86f9f3e80040
mateusz.kozak <koziu92@gmail.com>
2017-07-08 10:12:54 +0200

    Cleared

--- PatientsImporter/Conventers/IConventer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PatientsImporter.Conventers
-{
-  public interface IConventer<in TSource, out TResult> 
-  {
-    TResult Convert(TSource source);
-  }
-}
--- PatientsImporter/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PatientsImporter.MvcApplication" Language="C#" %>
